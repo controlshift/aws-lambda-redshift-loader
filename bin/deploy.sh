833a8aea5aa9e3b76933b20ef3ee69bf092fdd4e
anero <anero79@gmail.com>
2020-01-22 14:57:40 -0300

    New bucket with cross-region replication

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,4 +4,4 @@
 ver=`cat package.json | grep "version" | cut -d: -f2 | sed -e "s/[\"\,]//g" | tr -d '[:space:]'`
 token=AWSLambdaRedshiftLoader
 
-aws-vault exec changesprout -- aws s3 cp dist/$token-$ver.zip s3://changesprout-public/LambdaRedshiftLoader/$token-$ver.zip --acl public-read --region us-east-1;
+aws-vault exec changesprout -- aws s3 cp dist/$token-$ver.zip s3://changesprout-lambdas-us-east-1/LambdaRedshiftLoader/$token-$ver.zip --acl public-read --region us-east-1;
